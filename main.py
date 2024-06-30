3826ef6e9afd135ad59372689d6297534ba32be0
jdav892 <jadavila9@gmail.com>
2024-06-30 14:20:14 -0400

    error handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generator():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -27,16 +28,28 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
     
-    if website == "" or email == "" or password == "":
-        messagebox.showwarning(title="Oops", message="Don't leave any fields empty!")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n {email} "
-                               f"\nPassword: {password} \nIs it okay to save?")
-
-        if is_ok:
-            with open("data.txt", "a") as save_file:
-                save_file.write(f"{website} | {email} | {password}\n")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Don't leave any fields empty!")
+    else: 
+        try:
+            with open("data.json", "r") as save_file:
+                data = json.load(save_file)
+                
+        except FileNotFoundError:
+            with open("data.json", "w") as save_file:
+                json.dump(new_data, save_file, indent=4)
+        else:
+            data.update(new_data) 
+                
+            with open("data.json", "w"): 
+                json.dump(data, save_file, indent=4)
+        finally:
+            
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
         
