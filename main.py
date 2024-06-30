0680772fd8f03d47d3280aef949c5d30274d9014
jdav892 <jadavila9@gmail.com>
2024-06-29 23:52:36 -0400

    pretty useful

---
+++ main.py
@@ -0,0 +1,57 @@
+from tkinter import *
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    with open("data.txt", "a") as save_file:
+        save_file.write(f"{website} | {email} | {password}\n")
+        
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "Test@gmail.com")
+
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+add_button = Button(text="Add", width=36, height=1, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+password_button = Button(text="Generate Password")
+password_button.grid(column=2, row=3)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="100daysOfPy/passwordGen/logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+#canvas.grid(column=1, row=1)
+
+
+window.mainloop()
